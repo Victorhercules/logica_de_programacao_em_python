964988d3272eb5167abdb2b255e9321d61789d52
Victor Hercules <vhhercules@gmail.com>
2025-08-09 19:31:41 -0300

    Update Exemplo_f-string.py

--- Exemplo_f-string.py
+++ Exemplo_f-string.py
@@ -1,4 +1,4 @@
 nome = ("Victor")
 idade = ("19")
 print(f"Olá {nome}, fiquei sabendo que você fez {idade} anos recentemente.")
-# Saída: Olá Victor, fiquei sabendo que você fez 19 anos recentemente.
+# Saída: Olá Victor, fiquei sabendo que você fez 19 anos recentemente. 
