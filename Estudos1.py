d11fb703cbf26e81b741493fab792ca56100e384
Victor <vhhercules@gmail.com>
2025-08-09 19:28:35 -0300

    Update

--- Estudos1.py
+++ Estudos1.py
@@ -1,9 +1,9 @@
-nome = input("Qual é o seu nome? ") #pergunta o nome e armazena na variavel "nome"
-print() #pula uma linha
-idade = input("Qual é a sua idade? ") #pergunta a idade e armazena na variavel "idade"
+nome = input("Qual é o seu nome? ") # Pergunta o nome e armazena na variavel "nome"
+print() # Pula uma linha
+idade = input("Qual é a sua idade? ") # Pergunta a idade e armazena na variavel "idade"
 print()
-print("Olá,", nome) #mostra um texto e o que esta na variavel "nome"
+print("Olá,", nome) # Mostra um texto e o que esta na variavel "nome"
 print()
-aniversario = input(f"Ei {nome}, você disse que tem {idade} anos. Que dia você faz aniversário? ") #pergunta o dia do aniversário e guarda na variavel "aniversario"
+aniversario = input(f"Ei {nome}, você disse que tem {idade} anos. Que dia você faz aniversário? ") # Pergunta o dia do aniversário e guarda na variavel "aniversario"
 print()
-print(f"Que legal, {nome}! Então você faz aniversário no dia {aniversario}!") #mostra uma menssagem usando as variaveis "nome" e "aniversario"
+print(f"Que legal, {nome}! Então você faz aniversário no dia {aniversario}!") # Mostra uma menssagem usando as variaveis "nome" e "aniversario"
