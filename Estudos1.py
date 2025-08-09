9985b6b1600abebece7c3d0200e8dc3ece8d2cae
Victor <vhhercules@gmail.com>
2025-08-09 18:45:33 -0300

    Meus estudos de lógica de programação em python

--- Estudos1.py
+++ Estudos1.py
@@ -1,9 +1,9 @@
-nome = input("Qual é o seu nome? ")
+nome = input("Qual é o seu nome? ") #pergunta o nome e armazena na variavel "nome"
+print() #pula uma linha
+idade = input("Qual é a sua idade? ") #pergunta a idade e armazena na variavel "idade"
 print()
-idade = input("Qual é a sua idade? ")
+print("Olá,", nome) #mostra um texto e o que esta na variavel "nome"
 print()
-print("Olá,", nome)
+aniversario = input(f"Ei {nome}, você disse que tem {idade} anos. Que dia você faz aniversário? ") #pergunta o dia do aniversário e guarda na variavel "aniversario"
 print()
-aniversario = input(f"Ei {nome}, você disse que tem {idade} anos. Que dia você faz aniversário? ")
-print()
-print(f"Que legal, {nome}! Então você faz aniversário no dia {aniversario}!")
+print(f"Que legal, {nome}! Então você faz aniversário no dia {aniversario}!") #mostra uma menssagem usando as variaveis "nome" e "aniversario"
---
+++ Exemplo_join.py
@@ -0,0 +1,4 @@
+nomes = ["João", "Maria", "Pedro"]
+separador = ", "
+string_unida = separador.join(nomes)
+print(string_unida)  # Saída: João, Maria, Pedro
