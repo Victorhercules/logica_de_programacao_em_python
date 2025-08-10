783d16c176410df0a36ea83c38a9170594f98da4
Victor <vhhercules@gmail.com>
2025-08-10 01:11:36 -0300

    Update

--- Codigos_iniciais/Senhas_simples.py
+++ Codigos_iniciais/Senhas_simples.py
@@ -1,18 +1,26 @@
+
+# Cria uma função para pular uma linha
+def pular_linha():
+    print()
+
 # Cria uma senha e garante que não será vazia
 senha_criada = input("Crie uma senha: ")
 while senha_criada == "":
+    pular_linha()
     print("A senha não pode ser vazia!")
     senha_criada = input("Crie uma senha: ")
 
-print()
+pular_linha()
 
 # Garante que a senha digitada não está vazia ou errada
 senha_digitada = input("Digite sua senha para entrar: ")
 while senha_digitada == "" or senha_digitada != senha_criada:
     if senha_digitada == "":
+        pular_linha()
         print("Você não digitou a senha!")
     else:
+        pular_linha()
         print("Senha incorreta!")
     senha_digitada = input("Digite sua senha para entrar: ")
-
+pular_linha()
 print("Senha correta!")
