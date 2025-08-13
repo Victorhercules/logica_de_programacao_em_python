6aa5de2102e9ddb16bc5134d1ad6d45df284400a
Victor <vhhercules@gmail.com>
2025-08-13 01:56:57 -0300

    Update

--- Estudos_Python/Codigos_iniciais/acerte_o_numero.py
+++ Estudos_Python/Codigos_iniciais/acerte_o_numero.py
@@ -4,15 +4,31 @@ def pular_linha():
     print()
 
 numero = random.randint(1, 10)
+divisao = numero % 2
+
+if divisao == 0:
+    produto = "par"
+else:
+    produto = "ímpar"
 
 pular_linha()
 
-chute = int(input("Qual é o número que eu pensei? "))
+chute = int(input(f"Qual é o número que eu pensei?. "))
+
 pular_linha()
+
 while chute != numero:
+    if chute < numero:
         print(f"Você errou!")
+        print(f"O número é maior e {produto}.")
         pular_linha()
-        chute = int(input("Qual é o número que eu pensei? "))
+        chute = int(input(f"Qual é o número que eu pensei? "))
+        pular_linha()
+    else:
+        print(f"Você errou!")
+        print(f"O número é menor e {produto}.")
+        pular_linha()
+        chute = int(input(f"Qual é o número que eu pensei? "))
         pular_linha()
 
 print(f"Você acertou! o número é {numero}")
