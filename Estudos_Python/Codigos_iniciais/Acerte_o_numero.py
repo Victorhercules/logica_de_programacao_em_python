8c0375d7db8375d812c621bdfb4ce27e61e36749
Victor <vhhercules@gmail.com>
2025-08-14 17:53:46 -0300

    Adicionei mais uma validação

--- Estudos_Python/Codigos_iniciais/Acerte_o_numero.py
+++ Estudos_Python/Codigos_iniciais/Acerte_o_numero.py
@@ -1,22 +1,23 @@
 import random
 
-def pular_linha():
-    print()
-
 def validar():
-    pular_linha()
-    print(f"Você tem {vidas + 1} tentativas.")
-    chute_string = input("Qual é o número que eu pensei? ")
+    chute_string = input("\nQual é o número (de 1 a 10) que eu pensei? ")
     while not chute_string.isdigit():
-        print("Você não digitou um número")
-        pular_linha()
-        chute_string = input("Qual é o número que eu pensei? ")
+        print("Você não digitou um número!\n")
+        chute_string = input("Qual é o número (de 1 a 10) que eu pensei? ")
     chute_numero = int(chute_string)
+    while chute_numero > 10 or chute_numero < 1:
+        print("Você precisa digitar um número de 1 a 10!\n")
+        chute_string = input("Qual é o número (de 1 a 10) que eu pensei? ")
+        while not chute_string.isdigit():
+            print("Você não digitou um número!\n")
+            chute_string = input("Qual é o número (de 1 a 10) que eu pensei? ")
+        chute_numero = int(chute_string)
     return chute_numero
 
 numero = random.randint(1, 10)
 divisao_numero = numero % 2
-vidas = 4
+vidas = 5
 chute_numero = validar()
 
 if divisao_numero == 0:
@@ -24,23 +25,18 @@ if divisao_numero == 0:
 else:
     produto = "ímpar"
 
-pular_linha()
-
-while chute_numero != numero and vidas != 0:
-    if chute_numero < numero:
-        vidas = vidas - 1
-        tamanho = "maior"
+while chute_numero != numero and vidas > 0:
+    vidas -= 1
+    if vidas == 0:
+        break
+    elif chute_numero < numero:
+        print(f"Você errou! O número é maior e {produto}.\n")
     else:
-        vidas = vidas - 1
-        tamanho = "menor"
-    print(f"Você errou!")
-    pular_linha()
-    print(f"O número é {tamanho} e {produto}.")
+        print(f"Você errou! O número é menor e {produto}\n.")
+    print(f"Você tem {vidas} tentativas.")
     chute_numero = validar()
-    pular_linha()
 
 if chute_numero == numero:
-    print(f"Você acertou! O número é {numero}")
+    print(f"\nVocê acertou! O número é {numero}\n")
 else:
-    print(f"Você perdeu! O número era {numero}")
-pular_linha()
+    print(f"\nVocê perdeu! O número era {numero}\n")
